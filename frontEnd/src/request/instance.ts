f054830ef07f6599a3fa8327282f522cfc9d23bf
zang-liu-jun <2417170480@qq.com>
2023-11-05 15:24:27 +0800

    server

--- frontEnd/src/request/instance.ts
+++ frontEnd/src/request/instance.ts
@@ -1,4 +1,9 @@
 import axios from "axios";
 
-const instance = axios.create({});
+const baseURL='http://localhost:5173/'
+// const baseURL='http://118.31.56.248:7860/'
+
+const instance = axios.create({
+  baseURL
+});
 export default instance;
--- frontEnd/src/request/mock/video.ts
+++ frontEnd/src/request/mock/video.ts
@@ -1,4 +1,4 @@
-import instance from "./instance";
+import instance from "../instance";
 
 import { RecommendVideo } from "@/types/video";
 
