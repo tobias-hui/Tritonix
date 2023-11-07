52e0885d446abdc99d813e2834d31b6ad3a5f513
GHuiKai <aodesai19899278@13.com>
2023-11-07 08:26:49 +0800

    更改优化了前端的样式，加入tritonix ai的跳转按钮

--- frontEnd/src/components/sideBar/LeftSideBar.vue
+++ frontEnd/src/components/sideBar/LeftSideBar.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { SyncRefOptions } from "@vueuse/core";
 import SideBarItem from "./SideBarItem.vue";
 import { reactive } from "vue";
+import { ref } from 'vue';
 
 const sideList = reactive([
   {
@@ -8,8 +10,8 @@ const sideList = reactive([
     url: "/",
   },
   {
-    name: "二次元",
-    url: "/Otaku",
+    name: "知识",
+    url: "/",
   },
   {
     name: "体育",
@@ -19,27 +21,171 @@ const sideList = reactive([
     name: "宠物",
     url: "/pet",
   },
+  {
+    name: "娱乐",
+    url: "/",
+  },
+  {
+    name: "美食",
+    url: "/",
+  },
+  {
+    name: "二次元",
+    url: "/Otaku",
+  },
 ]);
+// 当前激活的项
+const currentActive = ref('');
+
+// 设置当前激活的项
+const selectItem = (itemName: string) => {
+  currentActive.value = itemName;
+};
+
 </script>
 
 <template>
   <div class="side-bar">
-    <SideBarItem
-      v-for="item in sideList"
-      :name="item.name"
-      :url="item.url"
-    ></SideBarItem>
+    <!-- 侧边栏容器 -->
+    <div class="sidebar-container">
+      <!-- 循环遍历侧边栏条目 -->
+      <SideBarItem
+        v-for="item in sideList"
+        :key="item.name"
+        :name="item.name"
+        :url="item.url"
+        :class="{ 'sidebar-item': true, active: item.name === currentActive }"
+        @click="selectItem(item.name)"
+      >
+        {{ item.name }}
+      ></SideBarItem>
+
+      <!-- 插入的图片 -->
+      <a href="https://colingo.ai/copilot/22e2bdc21c3a35ade2ec" class="image-link">
+        <img src="@/assets/toothless.png" alt="Toothless" class="sidebar-image">
+        <!-- 图片下方的文字 -->
+        <span class="image-text">Tritonix AI</span>
+      </a>
+
+      <!-- 新增的上传按钮 -->
+      <div class="special-button" @click="selectItem('upload')">
+        <!-- 使用img标签引入SVG文件 -->
+        <img class="icon" src="@/assets/add.svg" alt="Add">
+        <!-- 添加上传文字 -->
+        <span class="upload-text">上传</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .side-bar {
-  width: 80px;
+  width: 10%;
   height: 100%;
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
+  justify-content: flex-start;
   align-content:start;
-  // background-color: pink;
+  background-color: #191919; /* 侧边栏背景色 */
 }
+
+.sidebar-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #1C1C1C;
+  border-radius: 36px; /* 容器圆角 */
+  width: 80%; /* 与侧边栏同宽 */
+  height: 96%;
+  margin-top: 20px; /* 与顶部栏的距离 */
+  margin: auto;
+  padding: 10px 0; /* 容器内上下内边距 */
+}
+
+.sidebar-item {
+  display: flex; /* 设置为flex布局 */
+  flex-direction: column; /* 子元素垂直排列 */
+  align-items: center; /* 子元素水平居中对齐 */
+  justify-content: center; /* 子元素垂直居中对齐 */
+  background-color: #242424 !important; /* 按钮背景色 */
+  border-radius: 24px; /* 按钮圆角 */
+  color: #FFFFFF; /* 按钮文字颜色 */
+  font-size: 24px;
+  font-weight: 400; /* 设置字体字重为普通 */
+  font-family: 'Microsoft YaHei', '微软雅黑', 'Heiti SC', '黑体', sans-serif;
+  margin: 12px auto; /* 修改为 'auto' 实现水平居中 */
+  padding: 10px 0; /* 按钮上下内边距 */
+  width: 60%; /* 按钮宽度 */
+  height: 4%;
+  text-align: center; /* 文字居中 */
+  cursor: pointer; /* 鼠标悬停时的指针样式 */
+  transition: background-color 0.3s; /* 背景色变化的过渡效果 */
+  &:hover {
+    background-color: #656567 !important;
+    color: #CAFF33; /* 鼠标悬停时的按钮背景色 */
+  }
+  &.active {
+    color: #CAFF33; /* 激活状态的文字颜色 */
+  }
+}
+
+.image-link {
+  display: block; /* 块级元素，使链接填满容器宽度 */
+  margin: 32px auto; /* 上下外边距，根据需要调整 */
+  text-align: center; /* 使图片在链接内居中 */
+  transition: background-color 0.3s; /* 添加过渡效果 */
+  border-radius: 16px; /* 添加圆角效果 */
+
+  &:hover {
+    background-color: #CAFF33; /* 鼠标悬停时的背景色 */
+  }
+}
+
+.sidebar-image {
+  max-width: 100%; /* 限制图片最大宽度 */
+  max-height: 220px; /* 增加图片最大高度 */
+  margin-bottom: 16px; /* 在图片和上传按钮之间增加空间 */
+}
+
+.image-text {
+  color: #CAFF33; /* 文字颜色 */
+  font-size: 24px; /* 文字大小 */
+  font-weight: 400; /* 设置字体字重为普通 */
+  font-family: 'Microsoft YaHei', '微软雅黑', 'Heiti SC', '黑体', sans-serif;
+  text-align: center; /* 文字居中 */
+  display: block; /* 块级元素，使文本在新的一行显示 */
+}
+
+
+.special-button {
+  background-color: #242424; /* 按钮背景色 */
+  border-radius: 18px; /* 按钮圆角 */
+  width: 70%; /* 按钮宽度 */
+  height: 12%;
+  display: flex;
+  flex-direction: column; /* 使用flex布局使内容居中 */
+  align-items: center; /* 垂直居中 */
+  justify-content: center; /* 水平居中 */
+  margin: 12px auto; /* 上下外边距 */
+  margin-top: auto;
+  cursor: pointer; /* 鼠标悬停时的指针样式 */
+}
+
+.icon {
+  fill: white; /* SVG图标颜色 */
+  width: 80px; /* SVG图标宽度 */
+  height: 80px; /* SVG图标高度 */
+  margin-bottom: 4px;
+}
+.upload-text {
+  color: white; /* 文字颜色 */
+  font-size: 16px; /* 文字大小 */
+  font-weight: 400; /* 设置字体字重为普通 */
+  font-family: 'Microsoft YaHei', '微软雅黑', 'Heiti SC', '黑体', sans-serif;
+  text-align: center; /* 文字居中 */
+  &:hover {
+    color: #CAFF33; /* 鼠标悬停时的按钮背景色 */
+  }
+}
+
 </style>
--- frontEnd/src/components/topBar/TopBar.vue
+++ frontEnd/src/components/topBar/TopBar.vue
@@ -1,13 +1,144 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 引入logo图片
+import logo from '@/assets/logo.png'; // @ 是 src/ 目录的别名
+</script>
 
 <template>
-  <div class="top-bar"></div>
+  <div class="top-bar">
+    <div class="bar-container">
+      <!-- Logo图片 -->
+      <img :src="logo" alt="Logo" class="logo" />
+      <!-- 品牌文字 -->
+      <span class="brand">Tritonix 潮汐智螈</span>
+      <!-- 搜索框和放大镜图标 -->
+      <div class="search-container">
+        <input type="text" class="search-box" placeholder="搜索你感兴趣的内容" />
+        <button class="search-button">
+          <img src="@/assets/search.svg" alt="搜索" />
+        </button>
+      </div>
+      <!-- 登录和注册按钮（根据你的需求添加） -->
+      <button class="signup-button">注册</button>
+      <button class="login-button">登录</button>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .top-bar {
   width: 100%;
-  height: 70px;
-  background-color: darkgrey;
+  height: 120px;
+  background-color: #191919; /* 背景色 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
+
+.bar-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #242424; /* 你可以根据需要更改这个颜色 */
+  border-radius: 36px; /* 圆角的大小 */
+  padding: 10px 30px; /* 内边距 */
+  width: 82%;
+  height: 48px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  /* 根据需要添加更多的样式，如阴影等 */
+}
+
+/* 搜索容器样式 */
+.search-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 500px;
+  width: 600px; /* 设置宽度为70% */
+  height: 92%;
+  position: relative;
+  //max-width: 600px; /* 你可以设置一个最大宽度 */
+}
+
+/* 搜索框样式 */
+.search-box {
+  padding: 8px 16px;
+  background-color: #242424; /* 搜索框背景颜色 */
+  border: 2px solid #FFFFFF; /* 搜索框边框颜色 */
+  border-radius: 14px; /* 圆角边框 */
+  font-size: 16px; /* 文本大小 */
+  font-family: 'Helvetica Neue', Arial, sans-serif;
+  color: #FFFFFF; /* 输入文本颜色 */
+  outline: none; /* 移除焦点轮廓 */
+  width: 100%; /* 宽度 - 可根据需要调整 */
+  //max-width: 300px; /* 最大宽度 */
+  box-sizing: border-box; /* 盒模型定义 */
+}
+
+/* 搜索按钮样式 */
+.search-button {
+  background-color: transparent; /* 透明背景 */
+  border: none; /* 无边框 */
+  cursor: pointer;
+  position: absolute;
+  right: 20px; /* 右对齐 */
+  top: 50%; /* 垂直居中 */
+  transform: translateY(-50%); /* 垂直居中的微调 */
+}
+
+/* 搜索图标样式 */
+.search-button img {
+  width: 20px; /* 图标大小 */
+  height: 20px; /* 图标大小 */
+}
+
+
+.logo-container {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  height: 70px; /* 或者你希望的任何尺寸 */
+  margin-right: 10px; /* logo和文字之间的间距 */
+}
+
+.brand {
+  color: white;
+  font-size: 24px;
+  font-weight: 400; /* 设置字体字重为普通 */
+  font-family: 'Microsoft YaHei', '微软雅黑', 'Heiti SC', '黑体', sans-serif; /* 设置字体 */
+  margin-right: auto;
+  /* 其他你需要的样式 */
+}
+
+.signup-button {
+  margin-left: 10px;
+  padding: 5px 15px;
+  background-color: #242424;
+  color: #FFFFFF; /* 登录文字颜色 */
+  font-weight: 400; /* 设置字体字重为普通 */
+  font-family: 'Microsoft YaHei', '微软雅黑', 'Heiti SC', '黑体', sans-serif;
+  border: 2px solid #242424; /* 边框颜色和大小 */
+  border-radius: 30px;
+  cursor: pointer;
+  &:hover {
+    background-color: #656567; /* 按钮悬停效果颜色 */
+  }
+}
+
+.login-button {
+  margin-left: 10px;
+  padding: 5px 15px;
+  background-color: #242424; /* 按钮背景颜色 */
+  color: #CAFF33; /* 登录文字颜色 */
+  font-weight: 400; /* 设置字体字重为普通 */
+  font-family: 'Microsoft YaHei', '微软雅黑', 'Heiti SC', '黑体', sans-serif;
+  border: 2px solid #CAFF33; /* 边框颜色和大小 */
+  border-radius: 30px;
+  cursor: pointer;
+  &:hover {
+    background-color: #656567; /* 按钮悬停效果颜色 */
+  }
+}
+
 </style>
