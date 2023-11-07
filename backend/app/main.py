937b819a5820472f9ebcf5f6ea2f01a70a84b981
GHuiKai <aodesai19899278@13.com>
2023-11-07 13:01:42 +0800

    后端解决跨域请求的问题

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import HTTPException, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.video import router as video_router
 from app.api.user import router as user_router
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(video_router, prefix="/api/v1")
 
